bd44c302e5e67319e7a93e0fa177c15a55871bd6
Alexander Ashavsky <Ashavsky@gmail.com>
2016-03-01 23:53:06 -0500

    Added Quiz Tables, Started User Tables
    Multiple files for working with tables - can create them locally in your
    own database or log into mine.

---
+++ QuizIt/SQL/QuizSampleInserts.sql
@@ -0,0 +1,37 @@
+USE c_cs108_ashavsky
+
+INSERT INTO QuizSummary VALUES
+  (null,'Quiz One','Description Of Quiz One', 21, '19881221'),
+  (null,'Quiz Two','Description Of Quiz Two', 42, '20010101'),
+  (null,'Quiz Three','Description Of Quiz Three', 100, '20101010')
+;
+
+INSERT INTO QuizHistory VALUES
+  (null,1, 21, 3, 5, '000200', '20160101'),
+  (null,1, 42, 4, 5, '000100', '20160102'),
+  (null,2, 100, 1, 5, '000200', '20160303')
+;
+
+INSERT INTO QuizStats VALUES
+	(1, 4, 40, 60, 2),
+	(2, 10, 100, 200, 5),
+	(1, 20, 200, 1000, 19)
+ ;
+ 
+INSERT INTO QuizQuestions VALUES
+	(null, 1, 'multchoice', 'Sample Question 1 for Quiz 1'),
+	(null, 1, 'picture', 'Sample Question 2 for Quiz 1'),	
+	(null, 2, 'multchoice', 'Sample Question 1 for Quiz 2'),
+	(null, 2, 'picture', 'Sample Question 2 for Quiz 2')
+ ;
+ 
+INSERT INTO QuizAnswers VALUES
+	(null, 1, 'multchoice', 'Sample Answer 1 for Question 1', true),
+	(null, 1, 'multchoice', 'Sample Answer 2 for Question 1', false),
+	(null, 2, 'picture', 'Sample Answer 3 for Question 2', true),
+	(null, 2, 'picture', 'Sample Answer 4 for Question 2', false),
+	(null, 3, 'multchoice', 'Sample Answer 5 for Question 3', true),
+	(null, 3, 'multchoice', 'Sample Answer 6 for Question 3', false),
+	(null, 4, 'picture', 'Sample Answer 6 for Question 4', true),
+	(null, 4, 'picture', 'Sample Answer 7 for Question 4', false)
+;
