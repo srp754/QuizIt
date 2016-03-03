22a4e9c6391530a2a6572adf6b5420fcc5d31455
Alexander Ashavsky <Ashavsky@gmail.com>
2016-03-03 18:28:41 -0500

    Uploaded Database Class with more methods
    This is last upload I'll probably do here, from here on it will be
    connecting directly to your classes

--- QuizIt/SQL/DatabaseTasks.java
+++ QuizIt/SQL/DatabaseTasks.java
@@ -2,6 +2,7 @@ package QuizIt;
 
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.ResultSetMetaData;
 import com.mysql.jdbc.Statement;
 
 import java.sql.DriverManager;
@@ -17,6 +18,58 @@ public class DatabaseTasks
     static String password = MyDBInfo.MYSQL_PASSWORD;
     static String server = MyDBInfo.MYSQL_DATABASE_SERVER;
     static String database = MyDBInfo.MYSQL_DATABASE_NAME;
+
+    public static ResultSet GetResultSet(Connection con, String tableName, String parameterName, int parameterValue)
+    {
+        ResultSet rs = null;
+
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Statement stmt = (Statement) con.createStatement();
+            stmt.executeQuery("USE " + database);
+
+            String query = String.format("Select * from %1$s WHERE %2$s = %3$d;", tableName, parameterName, parameterValue);
+//            String query = String.format("Select UserDetail.UserName from UserFriends join UserDetail on UserFriends.FriendId = UserDetail.UserId WHERE UserFriends.UserId = 1;");
+            rs = stmt.executeQuery(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return rs;
+    }
+
+    public static ResultSet GetResultSet(Connection con, String tableName)
+    {
+        ResultSet rs = null;
+
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Statement stmt = (Statement) con.createStatement();
+            stmt.executeQuery("USE " + database);
+
+            String query = String.format("Select * from %1$s;", tableName);
+//            String query = String.format("Select UserDetail.UserName from UserFriends join UserDetail on UserFriends.FriendId = UserDetail.UserId WHERE UserFriends.UserId = 1;");
+            rs = stmt.executeQuery(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return rs;
+    }
 
     public static ResultSet GetResultSet(Connection con)
     {
@@ -58,6 +111,27 @@ public class DatabaseTasks
                 String name = rs.getString("UserName");
                 System.out.println("Friend Name: " + name);
             }
+
+            ResultSet rs2 = GetResultSet(conNew, "UserDetail");
+            ResultSetMetaData rsmd = (ResultSetMetaData) rs2.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            for (int i = 1; i <= columnsNumber; i++)
+            {
+                System.out.print(rsmd.getColumnName(i) + ",  ");
+                if (i == columnsNumber) System.out.println(";");
+            }
+
+            while (rs2.next()) {
+                for (int i = 1; i <= columnsNumber; i++)
+                {
+                    String columnValue = rs2.getString(i);
+                    System.out.print(columnValue + ", ");
+                    if (i == columnsNumber) System.out.println(";");
+                }
+                System.out.println("");
+            }
+
             conNew.close();
         }
         catch (SQLException e)
